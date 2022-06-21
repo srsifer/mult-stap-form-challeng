6590924ff85dfcc7d21e45106fbbb8b5dd5a18c4
igor s fernandes <igorsife80@gmail.com>
2022-06-21 11:51:30 -0300

    feat: cria estilização inicial do modal

---
+++ src/styles/ModalStyles.jsx
@@ -0,0 +1,11 @@
+export const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    height: 'max-content',
+  },
+};  
