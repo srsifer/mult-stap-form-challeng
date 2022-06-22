b34d1c69e86cb76ed281d0792ab9dc9435da022b
igor s fernandes <igorsife80@gmail.com>
2022-06-22 19:37:39 -0300

    styles: componentiza o modal de dados do usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import React, {  useState } from 'react'
-import Modal from 'react-modal';
 import { useSelector } from 'react-redux'
+import DataModalUser from './components/DataModalUser';
 import  Menu  from './components/Menu'
-import { customStyles } from './styles/ModalStyles'
+
 
 function App() {
- const [showuserList, setShowUserList] = useState(true)
- const [showUserData, setShowUserData]= useState(false)
+    const [showuserList, setShowUserList] = useState(true)
 
- const storeRedux = useSelector(({UserStore}) => (UserStore.users))
 
   const toggleList = () => {
     setShowUserList(!showuserList)
   }
-
-  const toggleUserData = () => {
-    setShowUserData(!showUserData)
-  }
-
+  const storeRedux = useSelector(({UserStore}) => (UserStore.users))
   return (
     <>
-      <h1>multi Steps Form</h1>
+      <h1>Multi Steps Form</h1>
       <Menu ShowUserList={toggleList}/>
       {
          storeRedux == undefined || storeRedux == []
@@ -30,36 +24,11 @@ function App() {
            {
              storeRedux.map((client)=> {
                return (
-               <li 
-                key={client.id}
-               >
-                 <label name="user">
-                   <p>{`Nome: ${client.name} ${client.lastname}  email: ${client.email}`} </p>
-                    <button 
-                    hidden={true}
-                    id='user'
-                    onClick={toggleUserData}
-                    >
-                     visualizar dados
-                   </button>
-                 </label>
-                <Modal
-                  isOpen={showUserData}
-                  style={customStyles}
-                   contentLabel="create user modal"
-               >
-                  <h3>{`${client.name} ${client.lastname}`}</h3>
-                  <p>{client.email}</p>
-                  <p>{client.cell}</p>
-                  <p>{client.cep}</p>
-                  <p>{client.adress}</p>
-                  <p>{client.adresstwo}</p>
-                  <p>{client.date}</p>
-                  <p>{client.cpf}</p>
-                  <p>{client.salary}</p>
-                  <button onClick={() => toggleUserData()}>fechar</button>
-               </Modal>
-               </li>)
+               <DataModalUser
+                 key={client.id}
+                 client={client}
+                />
+               )
              })
            }
           </ul>
---
+++ src/components/DataModalUser.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { customStyles } from '../styles/ModalStyles';
+import PropTypes from 'prop-types'
+
+function DataModalUser({client}) {
+  const { 
+    name,
+    lastname,
+    email,
+    cell,
+    cep,
+    adress,
+    adresstwo,
+    date,
+    cpf,
+    salary
+  } = client;
+  const [showUserData, setShowUserData] = useState(false)
+
+  const toggleUserData = () => {
+    setShowUserData(!showUserData)
+  }
+
+  return (
+    <li>
+      <label name="user">
+        <p>{`Nome: ${name} ${lastname}  email: ${email}`} </p>
+        <button 
+          hidden={true}
+          id='user'
+          onClick={toggleUserData}
+        >
+          visualizar dados
+        </button>
+      </label>
+      <Modal
+        isOpen={showUserData}
+        style={customStyles}
+        contentLabel="create user modal"
+      >
+        <h3>{`${name} ${lastname}`}</h3>
+        <p>{email}</p>
+        <p>{cell}</p>
+        <p>{cep}</p>
+        <p>{adress}</p>
+        <p>{adresstwo}</p>
+        <p>{date}</p>
+        <p>{cpf}</p>
+        <p>{salary}</p>
+        <button onClick={() => toggleUserData()}>fechar</button>
+      </Modal>
+    </li>
+  )
+}
+
+DataModalUser.propTypes={
+  client: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    lastname: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    cell: PropTypes.string.isRequired,
+    cep: PropTypes.string.isRequired,
+    adress: PropTypes.string.isRequired,
+    adresstwo: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    cpf: PropTypes.string.isRequired,
+    salary: PropTypes.number.isRequired,
+  }).isRequired
+}
+
+export default DataModalUser;
