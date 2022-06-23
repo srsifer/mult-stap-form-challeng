39c5f6d2a569f1e4176c89f55e9e07713ef3c038
igor s fernandes <igorsife80@gmail.com>
2022-06-23 16:37:30 -0300

    feat: cria um stado pra controlar a estilização do componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,35 @@ import { useSelector } from 'react-redux'
 import DataModalUser from './components/DataModalUser';
 import  Menu  from './components/Menu'
 import { BodyAppSection, ClientListStyles, HeaderApp } from './styles/AppStyles';
+import { CgMenu } from "react-icons/cg";
 
 
 function App() {
   const [showuserList, setShowUserList] = useState(true)
+  const [showMenu, setShowMenu] = useState(false)
   const storeRedux = useSelector(({UserStore}) => (UserStore.users))
 
   const toggleList = () => {
     setShowUserList(!showuserList)
     localStorage.setItem('allUsers', JSON.stringify(storeRedux))
   }
+
+
+   const toggleShowMenu = () => {
+     setShowMenu(!showMenu)
+   }
   return (
     <>
     <HeaderApp>
+      <p></p>
       <h1>Multi Steps Form</h1>
+      <label name="checkMenu">
+      <input hidden type="checkbox" onClick={toggleShowMenu} id="checkMenu"/>
+        <CgMenu/>
+      </label>
     </HeaderApp>
     <BodyAppSection>
-      <Menu ShowUserList={toggleList}/>
+      <Menu ShowUserList={toggleList} showMenu={showMenu}/>
       {
         storeRedux == undefined || storeRedux == []
         ? <h2>nenhum cliente cadastrado </h2>
