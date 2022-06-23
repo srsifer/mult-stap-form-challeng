e9a90544ef250219afea9bb692dbaf0a73e33dc5
igor s fernandes <igorsife80@gmail.com>
2022-06-23 12:24:47 -0300

    featStyles: cria o globalStyle resetCss

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { Provider } from 'react-redux'
-import store from './reduxtollkit/store'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './reduxtollkit/store';
+import GlobalStyles from './styles/globalStyles';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>
