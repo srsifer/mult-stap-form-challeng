694b36059d86221fb9dccded517631d47dd2e7b2
igor s fernandes <igorsife80@gmail.com>
2022-06-23 17:08:24 -0300

    featStyles: cria component de estilização pra dataModalUser

---
+++ src/styles/DataModalUserStyles.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const MainDataModalStyles = styled.main`
+  text-align: start;
+  font-size: 18px;
+  font-weight: bolder;
+  div{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 1em;
+  };
+  hr {
+    margin: 5px;
+    border: 1px solid white;
+
+  }
+`;
